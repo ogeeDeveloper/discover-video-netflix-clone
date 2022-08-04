0c7b445909c26863cb3aca86267ca88aef99b4da
ogeedeveloper <omarogrant202@hotmail.com>
2022-08-04 13:35:08 -0500

    Created the structure for the Banner component

---
+++ components/Banner.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import React from 'react'
+
+interface Props {
+    // any props that come into the component
+    title:string,
+    subtitle:string,
+    imgURL:string
+}
+
+const Banner: React.FunctionComponent<Props> = (props) => {
+    const {title,subtitle,imgURL} = props
+    const handleOnPlay = ()=>{
+        console.log("You clicked the play button")
+    }
+  return (
+    <div>
+        <h3>{title}</h3>
+        <h3>{subtitle}</h3>
+        <button onClick={handleOnPlay}>Play</button>
+        <div className={`bg-[url(${imgURL})] w-full h-full absolute bg-cover bg-center`}></div>
+        {/* <Image 
+        src = {`${imgURL}`}
+        layout="fill"
+        objectFit="cover"
+        alt='Banner image'
+        /> */}
+    </div>
+  )
+}
+
+export default Banner
